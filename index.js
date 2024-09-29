42d67ade46682fa3886d0711059d68301dca8b7d
Morgan <mashgrant7@gmail.com>
2024-09-29 14:38:50 +0300

    created myfile.md

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+console.log( multiply )
 
-// Write your code here
+var random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+
+const num3 = 5
+const num4 = 29
+const mod = num4 % num3
+mod === 4
+console.log( mod )
+
+const max = Math.max(17, 10, 20)
+console.log( max )
